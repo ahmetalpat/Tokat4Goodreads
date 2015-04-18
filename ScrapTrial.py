854762cf34affb96e24626066608920c63d4b42b
ahmetalpat <ahmetalpat@gmail.com>
2015-04-18 12:07:46 +0300

    Create ScrapTrial.py
    Messing around script

---
+++ ScrapTrial.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import bs4
+import requests
+from lxml import html
+import requests
+
+
+root_url = 'http://www.toplukatalog.gov.tr'
+index_url = root_url + '//index.php?_f=1&the_page=&cwid=2&keyword=9789757004677&tokat_search_field=4&order=0&command=Tara#alt'
+
+def get_book_names():
+    response = requests.get(index_url)
+    soup = bs4.BeautifulSoup(response.text)
+    print (soup)
+#   return [a.attrs.get('href') for a in soup.select('div.video-summary-data a[href^=/video]')]
+
+print(get_book_names())
+#print()
+
